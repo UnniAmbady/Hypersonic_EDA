395622e2f8b57e20e5be1e3498895e04fa698ff5
Ambady <ambady1960@hotmail.com>
2025-09-15 21:50:25 +0800

    Create __init__.py

---
+++ hypersonic/__init__.py
@@ -0,0 +1,5 @@
+from . import notebook_builder
+from .notebook_builder import build_notebook, main
+
+__all__ = ["notebook_builder", "build_notebook", "main"]
+__version__ = "0.1.0" #dated 15 Oct 2025
